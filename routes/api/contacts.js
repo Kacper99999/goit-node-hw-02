e014960fa3dee0676f60721549c75b6bcd26c849
Kardiologpythona <91539787+Kardiologpythona@users.noreply.github.com>
2024-11-13 17:24:31 +0100

    Commit

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,9 +1,23 @@
-const express = require('express')
+const { error } = require('console');
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+const __dirname = path.dirname(__filename);
+const contactPath = path.join(__dirname,"models","contacts.json");
 
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.get('/api/contacts', async (req, res, next) => {
+  try{
+    const data = await fs.readFile(contactPath,"utf-8");
+    const contacts = JSON.parse(data);
+
+    res.json(contacts);
+  }
+  catch(error){
+    next(error)
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
